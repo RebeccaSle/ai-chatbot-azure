41e5f4874af9ca5421520780103144c159732ea2
RebeccaSle <rebeccasleiman03@gmail.com>
2025-11-10 01:53:57 +0200

    Add code-based index pipeline (automation task)

---
+++ automated_index.py
@@ -0,0 +1,9 @@
+##This file will contain all the automation logic:
+
+##Read document from Azure Blob Storage
+
+##Use Azure Document Intelligence to extract text
+
+##Generate embeddings using Azure OpenAI
+
+##Create an index and upload embeddings to Azure Search
